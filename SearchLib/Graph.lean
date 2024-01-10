6c7c29ee19eadc1c437f195310b94f7e058672e3
Justin Chiu <chiu.justin.t@gmail.com>
2024-01-10 11:14:28 -0500

    graph struggles

--- SearchLib/Graph.lean
+++ SearchLib/Graph.lean
@@ -3,15 +3,24 @@ namespace Graph
 structure Edge (β: Type) where
   target: Nat
   weight: β
+  deriving Repr
 
 structure Vertex (α: Type) (β: Type) where
   payload: α
   adjacencyList: Array (Edge β)
+  deriving Repr
 
 structure Graph (α: Type) (β: Type) where
   vertices: Array (Vertex α β)
+  deriving Repr
 
 end Graph
 
-def x := Graph.Edge.mk 1 2
-#check x
+def v₁: Graph.Vertex Nat Nat := { payload := 1, adjacencyList := #[] }
+def v₂: Graph.Vertex Nat Nat := { payload := 2, adjacencyList := #[v₁] }
+def v₃: Graph.Vertex Nat Nat := { payload := 3, adjacencyList := #[v₂] }
+
+
+def chain_graph: Graph.Graph := { vertices := #[v₁, v₂, v₃] }
+
+#eval chain_graph
