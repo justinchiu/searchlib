b70dc788432c52fe487f142c823a4bab06e0b5b8
Justin Chiu <chiu.justin.t@gmail.com>
2024-01-11 21:05:35 -0500

    vscode struggles, graph simplification

--- SearchLib/Graph.lean
+++ SearchLib/Graph.lean
@@ -1,26 +1,11 @@
-namespace Graph
+universe v
 
-structure Edge (β: Type) where
-  target: Nat
-  weight: β
+inductive DiGraph (α: Type v) where
+  | leaf (value: α)
+  | node (value: α) (children: Array (DiGraph α))
   deriving Repr
 
-structure Vertex (α: Type) (β: Type) where
-  payload: α
-  adjacencyList: Array (Edge β)
-  deriving Repr
-
-structure Graph (α: Type) (β: Type) where
-  vertices: Array (Vertex α β)
-  deriving Repr
-
-end Graph
-
-def v₁: Graph.Vertex Nat Nat := { payload := 1, adjacencyList := #[] }
-def v₂: Graph.Vertex Nat Nat := { payload := 2, adjacencyList := #[v₁] }
-def v₃: Graph.Vertex Nat Nat := { payload := 3, adjacencyList := #[v₂] }
-
-
-def chain_graph: Graph.Graph := { vertices := #[v₁, v₂, v₃] }
-
-#eval chain_graph
+def v₃: DiGraph Nat := DiGraph.leaf 3
+def v₂: DiGraph Nat := DiGraph.node 2 #[v₃]
+def v₁: DiGraph Nat := DiGraph.node 1 #[v₂]
+#eval v₁
