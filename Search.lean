df5b1bf379bfcf7dacfaedaa849071cf4decb4a4
Justin Chiu <justinchiu@g.harvard.edu>
2024-01-13 23:36:59 -0500

    init lake project

---
+++ Search.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `Search` library.
+-- Import modules here that should be built as part of the library.
+import «Search».Basic
--- Search/Graph.lean
+++ Search/Graph.lean
@@ -8,4 +8,4 @@ inductive DiGraph (α: Type v) where
 def v₃: DiGraph Nat := DiGraph.leaf 3
 def v₂: DiGraph Nat := DiGraph.node 2 #[v₃]
 def v₁: DiGraph Nat := DiGraph.node 1 #[v₂]
-#eval v₁
+--#eval v₁
